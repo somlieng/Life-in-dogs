b53361fab8b9e4615afa712cb91d7b908e56e32e
Som Euakarn Liengtiraphan <eliengti@u.rochester.edu>
2020-10-24 15:24:41 -0400

    Made the canvas window size and loaded in the dog name JSON

--- sketch.js
+++ sketch.js
@@ -1,7 +1,26 @@
+let dogNames;
+
+function preload(){
+    let dogNameURL = "https://raw.githubusercontent.com/dariusk/corpora/master/data/animals/dog_names.json";
+    dogNames = loadJSON(dogNameURL);
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight); 
+    //create canvas full window size
 }
 
 function draw() {
-  background(220);
+    background(220);
+    let rand = dogNames.dog_names.length;
+    let randName = random(0,rand-1);
+    let name = dogNames.dog_names[floor(randName)];
+    print(name);
+    print (randName);
+    text(name,100,100);
+    noLoop();
+}
+
+function windowResized (){
+    resizeCanvas(windowWidth,windowHeight);
 }
